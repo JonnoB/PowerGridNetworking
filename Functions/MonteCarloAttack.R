487cc42b2fb5807b48e73f17c0c9aa6bf3320844
Jonathan Bourne <cheg_a_leg@hotmail.com>
2017-09-27 11:14:38 +0100

    MonteCarlo2
     IMproved the montecarlo function so that it produces it's own deletion matrix , takes any attackstrategy as long as it is fixed and can delete nodes or edges

--- Functions/MonteCarloAttack.R
+++ Functions/MonteCarloAttack.R
@@ -1,12 +1,26 @@
-MonteCarloAttack <- function(g, simulations = 100, DeletionMatrix, MinMaxComp, TotalAttackRounds, CascadeMode ){
+MonteCarloAttack <- function(g, simulations = 100, AttackStrategy, MinMaxComp, TotalAttackRounds, Target = Nodes, CascadeMode ){
+  #This function performs a montevcarlo simulation of attack the grid. It out puts a dataframe of graph statistics and the full
+  #deletion order for each simulation. The deletion order is the order in which the nodes were actually deleted, which is 
+  #different to the order they were planned to be deleted. Some nodes will be removed due to cascade effects or, 
+  #dead islanding. These nodes are then removed from the original deletion list and the next available node is deleted.
   
+  #g: an igraph objects
+  #simulations: the total number of simulations to perform
+  #Attackstrategy: The attack strategy expression, simply type in the name of the function without brackets e.g "RandomAttack"
+  #MinMaxComp: The minimum size of the maximum component, as a fraction, for the process to continue, the default is set to 0.8
+  #TotalAttackRounds: The maximum number of nodes to be removed before the process stops
+  #Target: wheather nodes or edges are being deleted
+  #CascadeMode: Whether the power flow equations will be used to check line-overloading or not
   Bigdf <- list()
-  
+  DeletionMatrix <- 1:simulations %>% map( ~
+                                   AttackStrategy(g, Target, Number)
+  )
+
   for(n in 1:simulations){ # ready for paralellization
     
     DeleteNodes <- DeletionMatrix[[n]]
 
-    FixedNodes <- quo(FixedStrategyAttack(g, DeleteNodes))
+    FixedNodes <- quo(FixedStrategyAttack(g, DeleteNodes, UQS(list(Target = Target))))
     
     sim <- paste0("Simulation_", n)
     print(sim)
@@ -28,3 +42,4 @@ MonteCarloAttack <- function(g, simulations = 100, DeletionMatrix, MinMaxComp, T
   
   return(Bigdf)
 }
+
---
+++ Functions/SummariseMonteCarlo.R
@@ -0,0 +1,18 @@
+SummariseMonteCarlo <- function(df){
+ df %>% 
+    mutate(PGfract = PowerGen/max(PowerGen)) %>%
+    group_by(NodesRemaining, Cascade) %>%
+    summarise(mean = mean(GCfract),
+              sd = sd(GCfract),
+              GC95 = quantile(GCfract, .95),
+              GC05 = quantile(GCfract, .05),
+              count = n(),
+              mPGfract = mean(PGfract),
+              PG95 = quantile(PGfract, .95),
+              PG05 = quantile(PGfract, .05)) %>%
+    ungroup %>%
+    group_by(Cascade) %>%
+    mutate(ID =n():1) %>%
+    ungroup
+  
+}
