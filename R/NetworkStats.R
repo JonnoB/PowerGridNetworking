757332999d66f87125ed28c85efd703837a480f1
Jonathan Bourne <cheg_a_leg@hotmail.com>
2018-09-27 13:08:18 +0100

    Slight update
    Added in Entropic degree and network stats as proper functions

--- R/NetworkStats.R
+++ R/NetworkStats.R
@@ -1,7 +1,15 @@
+#' NetworkStats
+#' Creates summary statistics of the network
+#'
+#' The function takes a graph and returns a set of common summary graph statistics to understand the graph topology.
+#' These statistics are, number of node, number of edges, number of components, Betweeness, Mean Degree, Assortivity, Clustering, and Mean Distance.
+#' @param g An igraph object
+#' @param colname A character string. The desired name of the output statistic column. the default is "value"
+
 NetworkStats <- function(g, colname = "value"){
   data_frame(Metric = c("Nodes", "Edges", "Components" ,"Betweenness", "Degree", "Assortativity", "Clustering", "Distance"),
              value = c(vcount(g), ecount(g), components(g)$no, mean(betweenness(g)),mean(degree(g)), assortativity(g, degree(g)),
-                       transitivity(g), 1)
+                       transitivity(g), mean_distance(g, directed = F))
   ) %>% setNames(c("Metric", colname))
 
   #This takes forever maybe should replace with *distances* or something else? and custome electrical distance function
--- R/RoundRemoved.R
+++ R/RoundRemoved.R
@@ -1,31 +1,44 @@
-#' Find Edge Deletion order
+#' Find Deletion order
 #'
-#' Attack round that each edge was deleted is  extracted from the network List of lists produced by AttackTheGrid
-#' 
-#' If not all edges were deleted during AttackTheGrid, these edges will be filled with NA
+#' Attack round that each edge/node was deleted is  extracted from the network List of lists produced by AttackTheGrid
+#'
+#' If not all edges/nodes were deleted during AttackTheGrid, these edges will be filled with NA
 #'
 #' @param ListofLists The output of AttackTheGrid, a list of lists containing igraphs.
+#' @param type Whether Edge or node order will be found. The default is Edge, any other value will mean nodes are used.
 #' @export
 #' @seealso \code{\link[PowerGridNetworking]{AttackTheGrid}}
 
-RoundRemoved <- function(ListofLists){
+RoundRemoved <- function(ListofLists, type = "Edge"){
 
-  RemovedDF <- data_frame(Edges = edge_attr(ListofLists[[1]][[1]], "Link"), RoundRemoved = NA )
-  
+  if(type =="Edge"){
+
+    Removetype <- function(ListHere){
+      ListHere %>% edge_attr(., "Link")
+    }
+
+  } else {
+    Removetype <- function(ListHere){
+      ListHere %>% vertex_attr(., "name")
+    }
+  }
+
+  RemovedDF <- data_frame(Edges = Removetype(ListofLists[[1]][[1]]), RoundRemoved = NA )
+
   FinalGraph <-ListofLists %>%
     map_dbl(~length(.x))
-  
+
   for(i in 2:length(FinalGraph)){
-    
-    EdgesRemoved <- difference(ListofLists[[i-1]][[FinalGraph[i-1]]], 
-                               ListofLists[[i]][[FinalGraph[i]]])  %>% 
-      edge_attr(., "Link")
-    
+
+    EdgesRemoved <- difference(ListofLists[[i-1]][[FinalGraph[i-1]]],
+                               ListofLists[[i]][[FinalGraph[i]]])  %>%
+      Removetype()
+
     RemovedDF <- RemovedDF %>%
       mutate(RoundRemoved = ifelse(Edges %in% EdgesRemoved, i-1, RoundRemoved))
-    
+
   }
 
   return(RemovedDF)
-  
+
 }
